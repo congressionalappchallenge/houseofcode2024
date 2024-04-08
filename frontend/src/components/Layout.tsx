804ca3d1d6297dfb332c99654487ee629aa77247
isabella hochschild <isabella.hochschild@dali.dartmouth.edu>
2024-04-08 17:15:50 -0400

    migrate from wasp framework and configure updated layout

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,52 @@
+import { AppShell, Center, Group, ActionIcon, Avatar, Button, Text, Container, rem } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconLayoutSidebarLeftCollapse, IconLayoutSidebarRightCollapse } from '@tabler/icons-react';
+import logo from "../assets/CAC-logo.svg";
+import Navbar from './Navbar';
+import classes from './layout.module.css';
+
+export function Layout() {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header className={classes.header}>
+        <Group h="100%" px="md" className={classes.logoGroup}>
+            <Avatar src={logo} radius="xs" className={classes.logoImg} />
+
+            {mobileOpened ? (<ActionIcon variant="transparent" aria-label="Collapse Navbar" onClick={toggleMobile}  hiddenFrom="sm" size="sm">
+                    <IconLayoutSidebarLeftCollapse style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+                </ActionIcon>) : (<ActionIcon variant="transparent" aria-label="Show Navbar" onClick={toggleMobile}  hiddenFrom="sm" size="sm">
+                    <IconLayoutSidebarRightCollapse style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+                </ActionIcon>)}
+            
+            {desktopOpened ? (<ActionIcon variant="transparent" aria-label="Collapse Navbar" size="lg" onClick={toggleDesktop}>
+                    <IconLayoutSidebarLeftCollapse style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+                </ActionIcon>) : (<ActionIcon variant="transparent" aria-label="Show Navbar" size="lg" onClick={toggleDesktop}>
+                    <IconLayoutSidebarRightCollapse style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+                </ActionIcon>)}
+        </Group>
+        <Center className={classes.headerTextContainer}>
+            <h1 className={classes.headerText}>Congressional App Challenge</h1>
+        </Center> 
+      </AppShell.Header>
+      <Container className={classes.layoutContainer}>
+        {(desktopOpened || mobileOpened) ? <Navbar /> : <div className={classes.dummyNav} />}
+        <AppShell.Main className={classes.mainContainer}>
+            <Container>
+                <Text>Current Tab Will Go Here!</Text>
+            </Container>
+        </AppShell.Main>
+      </Container>
+    </AppShell>
+  );
+}
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { Center, Tooltip, UnstyledButton, Stack, Avatar, AppShell, rem } from '@mantine/core';
+import {
+  IconHome2,
+  IconMapPinSearch,
+  IconDeviceDesktopAnalytics,
+} from '@tabler/icons-react';
+import logo from "../assets/CAC-logo.svg";
+import classes from './navbar.module.css';
+
+interface NavbarLinkProps {
+  icon: typeof IconHome2;
+  label: string;
+  active?: boolean;
+  onClick?(): void;
+}
+
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      <UnstyledButton onClick={onClick} className={classes.link} data-active={active || undefined}>
+        <Icon style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+const navLinkData = [
+  { icon: IconHome2, label: 'Home' },
+  { icon: IconMapPinSearch, label: 'Map' },
+  { icon: IconDeviceDesktopAnalytics, label: 'Table' },
+];
+
+export default function Navbar() {
+  const [active, setActive] = useState(0);
+
+  const links = navLinkData.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => setActive(index)}
+    />
+  ));
+
+  return (
+    <AppShell.Navbar p="md" className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Stack justify="center" gap={0}>
+          {links}
+        </Stack>
+      </div>
+    </AppShell.Navbar>
+  );
+}
